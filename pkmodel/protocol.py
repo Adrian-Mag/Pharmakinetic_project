29f8c1677b1cbe7c68eec2226295bb9440cc5adc
Adrian Mag <zcfbagx@ucl.ac.uk>
2022-10-20 21:17:09 +0300

    changed to my new protocol

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -1,6 +1,7 @@
 #
 # Protocol class
 #
+import numpy as np
 
 class Protocol:
     """A Pharmokinetic (PK) protocol
@@ -12,16 +13,32 @@ class Protocol:
         an example paramter
 
     """
-    def __init__(self,
-                 amount: float = 10.0,
-                 interval: int = None
-                 ):
-        self.amount = amount
-        self.interval = interval
+    def __init__(self, end_time, points, intervals, spikes):
+        self.time = np.linspace(0, end_time, points)
+        self.values = np.zeros(points)
         
-    def dose(self):
-        if self.interval is None:
-            return self.amount
-        else:
-            return 0
-
+        for interval in intervals:
+            self.__add_interval(interval)
+        for spike in spikes:
+            self.__add_spike(spike)
+        
+    def __add_interval(self, interval: dict):
+        for index, t  in enumerate(self.time):
+            if t >= interval['start'] and t <= interval['end']:
+                self.values[index] += interval['dose']
+    
+    def __add_spike(self, spike: dict):
+        dt = self.time[1] - self.time[0]
+        correct_dose = spike['dose']/dt
+        # find location of spike time
+        index = np.searchsorted(self.time, spike['time'])
+        self.values[index] += correct_dose
+        
+    def value(self, t):
+        return int(self.values[np.where(self.time == t)])
+        
+intervals = [{'start': 0, 'end': 0.1, 'dose': 1}, 
+             {'start': 0.5, 'end': 0.6, 'dose': 1},
+             {'start': 0.9, 'end': 1, 'dose': 2}]
+spikes = [{'time': 0.2, 'dose': 1},
+          {'time': 0.8, 'dose': 2}]
