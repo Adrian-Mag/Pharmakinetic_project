cabf45367c85d0993dd84ef28e3ce181f55cbf58
Sam Scivier <samscivier@gmail.com>
2022-10-20 13:56:26 +0100

    #13 add dose function

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -8,6 +8,14 @@ class Protocol:
     Parameters
     ----------
 
+    amount: float, optional
+        If interval == None: ng/h of drug steadily applied
+        If interval != None: ng of drug added every <interval>
+        time steps
+
+        Default is 10.0 ng/h OR 10.0 ng every
+        <interval> time steps
+
     interval: int, optional
         Time steps between dose applications,
         if using instantaneous dosing
@@ -17,18 +25,31 @@ class Protocol:
         Specify if using instantaneous dosing,
         meaning an instantaneous application
         of <amount> ng every <interval> time steps
-
-    amount: float, optional
-        If interval == None: ng/h of drug steadily applied
-        If interval != None: ng of drug added every <interval>
-        time steps
-
-        Default is 10.0 ng/h OR 10.0 ng every
-        <interval> time steps
-
     """
     def __init__(self,
-                 interval: int = None,
-                 amount: float = 10.0):
-        self.interval = interval
+                 amount: float = 10.0,
+                 interval: int = None
+                 ):
         self.amount = amount
+        self.interval = interval
+
+    def dose(self):
+        """The dose as a function of time
+
+        Returns
+        -------
+
+        dose: float
+            If using constant dosing (self.interval == None),
+            returns the dose amount in ng/hr
+
+            If using instantaneous dosing (self.interval != None),
+            returns 0
+            In the Solution() class, an instantaneous dose of
+            self.amount will be added to the 1st compartent (either
+            central or the dosing compartment) every self.interval
+        """
+        if self.interval is None:
+            return self.amount
+        else:
+            return 0
