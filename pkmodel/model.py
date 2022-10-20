c2aff727b44b6d48a95b02001ea6f67f70712afd
synapsidfan <spencer.pevsner@env-res.ox.ac.uk>
2022-10-20 11:21:30 +0100

    Updated test_model and cleaned up model.py

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -1,30 +1,30 @@
 #
 # Model class
 #
-
-# from numpy import NaN
-
-
 class Model:
     """A Pharmokinetic (PK) model
 
     Parameters
     ----------
+    Required:
+    V_c: Volume of the central compartment in mL
+    CL: The clearance/elimination rate from the central compartment in mL/h
 
-    value: numeric, optional
-        an example paramter
+    Optional:
+    K_a: Absorption rate for the subcutaneous dosing per hour
+    Q_p1: Transition rate between central compartment and peripheral
+    compartment 1 in mL/h
+    V_p1: Volume of peripheral compartment 1 in mL
+    Q_p2: Transition rate between central compartment and peripheral
+    compartment 2 in mL/h
+    V_p2: Volume of peripheral compartment 2 in mL
 
     """
-    def __init__(self, V_c, CL, K_a=None, Q_p1=None, Q_p2=None, V_p1=None,
+    def __init__(self, V_c, CL, K_a=None, Q_p1=None, V_p1=None, Q_p2=None,
                  V_p2=None):
         self.V_c = V_c
         self.CL = CL
-        self.K_a = K_a
-        self.Q_p1 = Q_p1
-        self.Q_p2 = Q_p2
-        self.V_p1 = V_p1
-        self.V_p2 = V_p2
-        self.opt_params = [K_a, Q_p1, Q_p2, V_p1, V_p2]
+        self.opt_params = [K_a, Q_p1, V_p1, Q_p2, V_p2]
 
     def construct_param_dict(self):
         # Construct a dictionary of the model parameters.
@@ -33,15 +33,10 @@ class Model:
             'CL': self.CL
         }
         # Add any optional parameters to the dictionary
-        i = 0
-        optional_param_name_list = ['K_a', 'Q_p1', 'Q_p2', 'V_p1', 'V_p2']
+        i = 2  # i starts at 2 so that it doesn't overwrite V_c and CL
+        optional_param_name_list = ['K_a', 'Q_p1', 'V_p1', 'Q_p2', 'V_p2']
         for param in self.opt_params:
             if param is not None:
                 param_dict[optional_param_name_list[i]] = param
                 i += 1
-    # def rhs(t, y, Q_p1, V_c, V_p1, CL, X):
-    #     q_c, q_p1 = y
-    #     transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
-    #     dqc_dt = dose(t, X) - q_c / V_c * CL - transition
-    #     dqp1_dt = transition
-    #     return [dqc_dt, dqp1_dt]
+        return param_dict
