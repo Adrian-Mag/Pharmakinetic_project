c4a2a74f002598fdc11d89d7f4fad42c24ed3eae
Sam Scivier <samscivier@gmail.com>
2022-10-21 16:26:18 +0100

    Added Adrian functionality changes

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -2,76 +2,108 @@
 # Model class
 #
 class Model:
-    """A Pharmacokinetic (PK) model
+    """A Pharmokinetic (PK) model
+    ---------------------------------------
+    INPUTS: dictionary describing the model
+    ----------------------------------------
+    name: str
+        This will be used for plotting the solutions
+    CL: float
+        [mL/h], the clearance/elimination rate from the
+        central compartment
+    Vc: float
+        [mL], the volume of the central compartment
+    ka: float, optional
+        [1/h], the “absorption” rate for the s.c dosing.
+        If a ka is not given in the dictionary, its default
+        value will be 0
+    Qpi: float, optional
+        [mL/h], the transition rate between central compartment
+        and peripheral compartments i, where i can be any positive
+        integer
+        By default there are no peripheral compartmentss
+    Vpi: float, optional
+        [mL], the volume of the peripheral compartments i,
+        where i can be any positive integer. Qpi and Vpi
+        must be present at the same time. If only one of them
+        is offered, the code will throw an error
+        By default there are no peripheral compartmentss
 
-    Parameters
-    ----------
-    Required:
-    Vc (float): Volume of the central compartment in mL
-    CL (float): The clearance/elimination rate from the central compartment in
-    mL/h
+    The dictionary should have the following template:
+    {
+        'name': name your model <str>, (for visualization purposes)
 
-    Optional:
-    K_a (float): Absorption rate for the subcutaneous dosing per hour
-    Q_p1 (float): Transition rate between central compartment and peripheral
-    compartment 1 in mL/h
-    V_p1 (float): Volume of peripheral compartment 1 in mL
-    Q_p2 (float): Transition rate between central compartment and peripheral
-    compartment 2 in mL/h
-    V_p2 (float): Volume of peripheral compartment 2 in mL
+        'CL': CL <float> [mL/h],
 
-    Important instructions:
-    For one peripheral compartment, assign values to Q_p1 and V_p1.
-    For two peripheral compartments, assign values to Q_p1, V_p1, Q_p2, and
-    V_p2
-    Do not assign Q_p2 and V_p2 without first assigning Q_p1 and V_p1!!
-    For subcutaneous dosing, assign a value to K_a
+        'Vc': Vc <float>, [mL]
 
-    Attributes
-    ----------
-    Peripherals (int): the number of peripheral compartments in the model
-    Subcutaneous (int): the number of subcutaneous dosing compartments in the
-    model
-    Vc: equivalent to the V_c parameter
-    CL: equivalent to the CL parameter
-    opt_params: a list containing the optional parameters listed above
-    Qp: a list containing all of the Qp values that are not None
-    Vp: a list containing all of the Vp values that are not None
+        'ka': ka <float>, [1/h] (if subcutaneous)
+
+        'Qp1': Qp1 <float>, [mL/h] (if one peripheral used)
+
+        'Vp1': Vp1 <float>, [mL] (if one peripheral used)
+
+        'Qp2': Qp1 <float>, [mL/h] (if two peripheral used)
+
+        'Vp2': Vp1 <float>, [mL] (if two peripheral used)
+
+        'Qp3': Qp1 <float>, [mL/h] (if three peripheral used)
+
+        'Vp3': Vp1 <float>, [mL] (if three peripheral used)
+        ... etc
+    }
+
+    **The peripheral compartmentss must be added starting from
+    1 and increasing in increments of 1 up to the maximum number
+    peripheral compartmentss used**
+
+    **At a minimum, the model must have a main compartiemnt with
+    CL and Vc given, and a name**
     """
-    def __init__(self, Vc, CL, K_a=None, Q_p1=None, V_p1=None, Q_p2=None,
-                 V_p2=None):
-        self.Vc = Vc
-        self.CL = CL
-        self.opt_params = [K_a, Q_p1, V_p1, Q_p2, V_p2]
+    def __init__(self, dict: dict):
+
+        if ("CL" not in dict):
+            raise ValueError('Please provide CL!')
+        elif ("Vc" not in dict):
+            raise ValueError('Please provide Vc!')
+        elif ("name" not in dict):
+            raise ValueError('Please provide name!')
+
+        if isinstance(dict['name'], str):
+            self.name = dict["name"]
+        else:
+            raise ValueError('name must be string')
+        if isinstance(dict['CL'], float) and \
+           isinstance(dict['Vc'], float):
+            self.CL = dict["CL"]
+            self.Vc = dict["Vc"]
+        else:
+            raise ValueError('CL and Vc must be floats')
+
         self.Qp = []
         self.Vp = []
-        if Q_p2 is not None:
-            self.peripherals = 2
-            self.Qp.extend([Q_p1, Q_p2])
-            self.Vp.extend([V_p1, V_p2])
-        elif Q_p1 is not None:
-            self.peripherals = 1
-            self.Qp.append(Q_p1)
-            self.Vp.append(V_p1)
-        else:
-            self.peripherals = 0
-        if K_a is None:
-            self.subcutaneous = 0
-            self.K_a = K_a
-        else:
-            self.subcutaneous = 1
+        self.ka = 0
 
-    def construct_param_dict(self):
-        # Construct a dictionary of the model parameters.
-        param_dict = {
-            'V_c': self.Vc,
-            'CL': self.CL
-        }
-        # Add any optional parameters to the dictionary
-        i = 0
-        optional_param_name_list = ['K_a', 'Q_p1', 'V_p1', 'Q_p2', 'V_p2']
-        for param in self.opt_params:
-            if param is not None:
-                param_dict[optional_param_name_list[i]] = param
-                i += 1
-        return param_dict
+        # Cycle through the input dictionary and separate
+        # the attributes
+        for key in dict:
+            if key[0] == "Q" and key[1] == "p":
+                if isinstance(dict[key], float):
+                    self.Qp.append(dict[key])
+                else:
+                    raise ValueError('All Qps must be floats')
+            if key[0] == "V" and key[1] == "p":
+                if isinstance(dict[key], float):
+                    self.Vp.append(dict[key])
+                else:
+                    raise ValueError('All Vps must be floats')
+
+        self.subcutaneous = 0
+
+        if "ka" in dict:
+            if dict['ka'] == 0:
+                raise ValueError('ka must be positive!')
+            self.subcutaneous = 1
+            self.ka = dict['ka']
+
+        self.peripherals = len(self.Qp)
--- pkmodel/tests/test_protocol.py
+++ pkmodel/tests/test_protocol.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 import pkmodel as pk
 
 
@@ -10,19 +11,22 @@ class ProtocolTest(unittest.TestCase):
         """
         Tests Protocol creation.
         """
-        protocol = pk.Protocol()
-        self.assertEqual(protocol.amount, 10.0)
-        self.assertEqual(protocol.interval, None)
+        end_time = 1
+        points = 101
+        intervals = [{'start': 0, 'end': 0.1, 'dose': 1},
+                     {'start': 0.5, 'end': 0.6, 'dose': 1},
+                     {'start': 0.9, 'end': 1, 'dose': 2}]
+        spikes = [{'time': 0.2, 'dose': 1},
+                  {'time': 0.8, 'dose': 2}]
+        my_protocol = pk.Protocol('protocol1', end_time, points,
+                                  intervals, spikes)
 
-    def test_dose(self):
-        """
-        Tests the dose() function of the Protocol class
-        """
-        protocol_1 = pk.Protocol(amount=15.0)
-        protocol_2 = pk.Protocol(amount=5.0, interval=100)
-        self.assertEqual(protocol_1.interval, None)
-        self.assertEqual(protocol_2.interval, 100)
-        self.assertEqual(protocol_1.amount, 15.0)
-        self.assertEqual(protocol_2.amount, 5.0)
-        self.assertEqual(protocol_1.dose(), 15.0)
-        self.assertEqual(protocol_2.dose(), 0)
+        assert ((my_protocol.time == np.linspace(0, 1, 101)).all())
+        self.assertEqual(my_protocol.value(0.1), 1)
+        self.assertEqual(my_protocol.value(0.2), 100)
+        self.assertEqual(my_protocol.value(0.8), 200)
+        self.assertEqual(my_protocol.value(0.82), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
