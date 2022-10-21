982d24c39ce508c0d2d11603178034deebd477e4
Adrian Mag <zcfbagx@ucl.ac.uk>
2022-10-21 13:14:31 +0300

    Deleted redundant model.py file

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -1,40 +1,81 @@
-"""     Parameters
+#
+# Model class
+#
+
+class Model:
+    """A Pharmokinetic (PK) model
+    -------
+    INPUTS: dictionary describing the model
+    The dictionary should have the following template:
+    {
+        'name': name <str>,
+        'CL': CL <float> [mL/h],
+        'Vc': Vc <float>, [mL]
+        'ka': ka <float>, [1/h] (if subcutaneous)
+        'Qp1': Qp1 <float>, [mL/h] (if one peripheral used)
+        'Vp1': Vp1 <float>, [mL] (if one peripheral used)
+        'Qp2': Qp1 <float>, [mL/h] (if two peripheral used)
+        'Vp2': Vp1 <float>, [mL] (if two peripheral used)
+        'Qp3': Qp1 <float>, [mL/h] (if three peripheral used)
+        'Vp3': Vp1 <float>, [mL] (if three peripheral used)
+        ... etc
+    }
+
+    !! The peripheral compartiments must be added starting from
+    1 and increasing in increments of 1 up to the maximum number
+    peripheral compartiments used !!
+
+    !! At a minimum the model must have a main compartiemnt with
+    CL and Vc given and a name !!
     ----------
-    Required:
-    Vc (float): Volume of the central compartment in mL
-    CL (float): The clearance/elimination rate from the central compartment in
-    mL/h
-	@@ -27,26 +27,33 @@ class Model:
-    Subcutaneous (int): the number of subcutaneous dosing compartments in the
-    model
- """
-def __init__(self, Vc, CL, K_a=None, Q_p1=None, V_p1=None, Q_p2=None,
-                 V_p2=None):
-        self.Vc = Vc
-        self.CL = CL
-        self.opt_params = [K_a, Q_p1, V_p1, Q_p2, V_p2]
+    OUTPUTS: model object that holds all the properties as attributes
+    ----------
+
+    """
+    def __init__(self, dict: dict):
+
+        if ("CL" not in dict):
+            raise ValueError('Please provide CL!')
+        elif ("Vc" not in dict):
+            raise ValueError('Please provide Vc!')
+        elif ("name" not in dict):
+            raise ValueError('Please provide name!')
+
+        if isinstance(dict['name'], str):
+            self.name = dict["name"]
+        else:
+            raise ValueError('name must be string')
+        if isinstance(dict['CL'], float) and \
+           isinstance(dict['Vc'], float):
+            self.CL = dict["CL"]
+            self.Vc = dict["Vc"]
+        else:
+            raise ValueError('CL and Vc must be floats')
+
         self.Qp = []
         self.Vp = []
-        if Q_p2 is not None:
-            self.peripherals = 2
-            self.Qp.append(Q_p1, Q_p2)
-            self.Vp.append(V_p1, V_p2)
-        elif Q_p1 is not None:
-            self.peripherals = 1
-            self.Qp.append(Q_p1)
-            self.Vp.append(V_p1)
-        else:
-            self.peripherals = 0
-        if K_a is None:
-            self.subcutaneous = 0
-            self.K_a = K_a
-        else:
-            self.subcutaneous = 1
+        self.ka = 0
 
-    def construct_param_dict(self):
-        # Construct a dictionary of the model parameters.
-        param_dict = {
-            'V_c': self.Vc,
-            'CL': self.CL
-        }
-        # Add any optional parameters to the dictionary
+        # Cycle through the input dictionary and separate
+        # the attributes
+        for key in dict:
+            if key[0] == "Q" and key[1] == "p":
+                if isinstance(dict[key], float):
+                    self.Qp.append(dict[key])
+                else:
+                    raise ValueError('All Qps must be floats')
+            if key[0] == "V" and key[1] == "p":
+                if isinstance(dict[key], float):
+                    self.Vp.append(dict[key])
+                else:
+                    raise ValueError('All Vps must be floats')
+
+        self.subcutaneous = 0
+
+        if "ka" in dict:
+            if dict['ka'] == 0:
+                raise ValueError('ka must be positive!')
+            self.subcutaneous = 1
+            self.ka = dict['ka']
+
+        self.peripherals = len(self.Qp)
--- pkmodel/run_file.py
+++ pkmodel/run_file.py
@@ -1,34 +1,10 @@
 import pkmodel as pk
 import numpy as np
 
-# User imputs --------------------------------------------- 
-# Models
-model1 = {'name': 'model1',
-          'CL': 1.0,
-          'Vc': 2.0,
-          'ka': 10.0,
-          'Qp1': 3.0,
-          'Vp1': 4.0}
-model2 = {'name': 'model2',
-          'CL': 1.0,
-          'Vc': 1.0,
-          'ka': 1.0,
-          'Qp1': 1.0,
-          'Vp1': 1.0}
-# Protocols
-end_time = 1
-points = 101
-intervals = [{'start': 0, 'end': 1, 'dose': 5}] 
-"""              {'start': 0.5, 'end': 0.6, 'dose': 1},
-             {'start': 0.9, 'end': 1, 'dose': 2}] """
-spikes = [{'time': 0.2, 'dose': 1},
-          {'time': 0.8, 'dose': 1}]
+# Create Models----------------------------------------
 
-# Build 
-my_model = pk.Model(model1)
-my_model2 = pk.Model(model2)
-my_protocol = pk.Protocol('protocol 1', end_time, points, intervals, spikes)
-my_protocol.show_graph()
-solution = pk.Solution(my_model, my_protocol)
-solution2 = pk.Solution(my_model2, my_protocol)
-compare([solution, solution2])
+# Create protocols-------------------------------------
+
+# Solve------------------------------------------------
+
+# Visualize and compare--------------------------------
